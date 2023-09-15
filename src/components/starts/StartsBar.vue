d925af0d4c68c368aa62afb58037b3ca34f21c16
bre 97 <by.sgatn3@Gmail.com>
2023-09-15 18:15:25 +0800

    fix: fix the brightness bug

--- src/components/starts/StartsBar.vue
+++ src/components/starts/StartsBar.vue
@@ -1,61 +1,14 @@
 <template>
-    <div class="w-full max-w-full h-12 window-border">
+    <div class="relative w-full h-12 element-border back-blur p-1">
 
         <!-- No need gap -->
-        <FlexLayout class="relative z-0 back-blur items-center justify-between h-full w-full overflow-clip p-1">
+        <FlexLayout class="relative w-full h-full items-center justify-between overflow-clip">
 
             <div class="flex-none w-1/4 flex justify-start"></div>
 
             <!-- StartButton -->
-            <ExpandLayout class="relative flex items-center justify-center w-1/4 flex-grow h-full aspect-square">
-                
-                <!-- Open the starts panel -->
-                <template v-slot:actions="{ isExpanded, setIsExpanded}">
-                    <div
-                        @click="setIsExpanded(!isExpanded)"
-                        class="rounded-md hover:border hover:bg-white/75 transition-all h-full aspect-square"
-                    >
-                        <StartsButton></StartsButton>
-                    </div>
-                </template>
-
-                <!-- the Starts Panel -->
-                <template v-slot:expanded-content="{ isExpanded, setIsExpanded }">
-
-                    <div
-                        class="full-screen fixed top-0 left-0 grid justify-center items-end pb-16"
-                        :class="[isExpanded ? 'active opacity-100' : 'opacity-0 select-none pointer-events-none -z-[1]']"
-                    >
-
-                        <fluent-card class="relative bottom-0 w-auto mx-auto max-w-2xl rounded-xl min-h-min h-auto max-h-[32rem] back-blur bg-transparent">
-
-                            <!-- Content in here -->
-                            <FlexLayout class="flex-col px-8 py-4 pt-8 gap-8 justify-between h-full">
-                                <FlexLayout class="flex-grow flex-col gap-4">
-                                    <Search></Search>
-                                    <PinnedApps></PinnedApps>
-                                </FlexLayout>
-                                
-                                <div class="flex-none space-y-4">
-                                    <fluent-divider class="bg-black/5 flex-none h-[1px]"></fluent-divider>
-                                    <FlexLayout class="justify-between items-center">
-                                        <SimpleUserProfile></SimpleUserProfile>
-                                        <ShutdownMenu></ShutdownMenu>
-                                    </FlexLayout>
-                                </div>
-                            </FlexLayout>
-                        </fluent-card>
-
-                        <!-- z-index set need near relative or fixed -->
-                        <div
-                            @click="setIsExpanded(false)"
-                            class="fixed left-0 bottom-0 w-screen h-screen"
-                            :class="[isExpanded ? 'opacity-100 -z-[1]' : 'opacity-0 select-none pointer-events-none -z-[1]']"
-                        ></div>
-                    </div>
-                </template>
-            </ExpandLayout>
-
+            <StartsButton></StartsButton>
+            
             <div class="flex-none w-1/4 h-full flex justify-end">
                 <Naviagtion></Naviagtion>
             </div>
@@ -68,10 +21,7 @@
 
 <script setup lang="ts">
 import StartsButton from './StartsButton.vue';
-import Search from './Search.vue';
-import PinnedApps from './PinnedApps.vue';
-import SimpleUserProfile from './SimpleUserProfile.vue';
-import ShutdownMenu from './ShutdownMenu.vue';
+
 import Naviagtion from '@/components/control/Navigation.vue';
 
 
@@ -79,21 +29,5 @@ import Naviagtion from '@/components/control/Navigation.vue';
 </script>
 
 <style lang="css" scoped>
-.active {
-    animation: activeAnimate 0.15s linear;
-}
-.inactive {
-    animation: activeAnimate 0.15s linear reverse;
-}
 
-@keyframes activeAnimate {
-    from {
-        transform: translateY(20px);
-        opacity: 0;
-    }
-    to {
-        transform: translateY(0px);
-        opacity: 1;
-    }
-}
 </style>
--- src/components/starts/StartsButton.vue
+++ src/components/starts/StartsButton.vue
@@ -1,11 +1,79 @@
 <template>
-    <md-icon class="active:scale-75 active:icon-filled w-full h-full grid place-content-center">
-        window
-    </md-icon>
+    <ExpandLayout class="relative flex items-center justify-center w-1/4 flex-grow h-full aspect-square">
+                
+        <!-- Open the starts panel -->
+        <template v-slot:actions="{ isExpanded, setIsExpanded}">
+            <div
+                @click="setIsExpanded(!isExpanded)"
+                class="rounded-md hover:border hover:bg-white/75 transition-all h-full aspect-square"
+            >
+                <md-icon class="active:scale-75 active:icon-filled w-full h-full grid place-content-center">
+                    window
+                </md-icon>
+            </div>
+        </template>
+
+        <!-- the Starts Panel -->
+        <template v-slot:expanded-content="{ isExpanded, setIsExpanded }">
+
+            <div
+                class="full-screen fixed top-0 left-0 grid justify-center items-end pb-16"
+                :class="[isExpanded ? 'active opacity-100' : 'opacity-0 select-none pointer-events-none']"
+            >
+
+                <fluent-card class="relative back-blur bottom-0 w-auto mx-auto max-w-2xl rounded-xl min-h-min h-auto max-h-[32rem] bg-transparent">
+
+                    <!-- Content in here -->
+                    <FlexLayout class="flex-col px-8 py-4 pt-8 gap-8 justify-between h-full">
+                        <FlexLayout class="flex-grow flex-col gap-4">
+                            <Search></Search>
+                            <PinnedApps></PinnedApps>
+                        </FlexLayout>
+                        
+                        <div class="flex-none space-y-4">
+                            <fluent-divider class="bg-black/5 flex-none h-[1px]"></fluent-divider>
+                            <FlexLayout class="justify-between items-center">
+                                <SimpleUserProfile></SimpleUserProfile>
+                                <ShutdownMenu></ShutdownMenu>
+                            </FlexLayout>
+                        </div>
+                    </FlexLayout>
+                </fluent-card>
+
+                <!-- z-index set need near relative or fixed -->
+                <div
+                    @click="setIsExpanded(false)"
+                    class="fixed left-0 bottom-0 w-screen h-screen -z-[1]"
+                    v-show="isExpanded"
+                ></div>
+            </div>
+        </template>
+    </ExpandLayout>
 </template>
 
 <script setup lang="ts">
-
+import Search from './Search.vue';
+import PinnedApps from './PinnedApps.vue';
+import SimpleUserProfile from './SimpleUserProfile.vue';
+import ShutdownMenu from './ShutdownMenu.vue';
 </script>
 
-<style scoped></style>
+<style scoped>
+.active {
+    animation: activeAnimate 0.15s linear;
+}
+.inactive {
+    animation: activeAnimate 0.15s linear reverse;
+}
+
+@keyframes activeAnimate {
+    from {
+        transform: translateY(20px);
+        opacity: 0;
+    }
+    to {
+        transform: translateY(0px);
+        opacity: 1;
+    }
+}
+</style>
