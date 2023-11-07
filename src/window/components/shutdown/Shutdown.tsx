58392782802252957dc97be894e858d26a8c3dba
bre 97 <by.sgatn3@Gmail.com>
2023-11-07 15:50:50 +0800

    refactor: refactor shutdown component

--- src/window/components/shutdown/Shutdown.tsx
+++ src/window/components/shutdown/Shutdown.tsx
@@ -1,6 +1,8 @@
+import { useSystemDispatch, useSystemSelector } from "@/store/store"
 import { setReadyToShutdown } from "@/store/systemSlice"
-import { Body2, Spinner, makeStyles } from "@fluentui/react-components"
-import { useEffect } from "react"
+import { WorkspaceProcessState, updatePropertyForAllProcessState } from "@/store/workspaceSlice"
+import { Body2, Button, Spinner, makeStyles, shorthands } from "@fluentui/react-components"
+import { useEffect, useState } from "react"
 import { useDispatch } from "react-redux"
 
 const useStyles = makeStyles({
@@ -13,7 +15,11 @@ const useStyles = makeStyles({
         backgroundColor: 'black',
         '& *': {
             color: 'white'
-        }
+        },
+        ...{
+            ...shorthands.overflow('auto'),
+            ...shorthands.padding('48px'),
+        },
     },
     spinnerPanel: {
         display: 'flex',
@@ -21,13 +27,96 @@ const useStyles = makeStyles({
         justifyContent: 'center',
         height: '100%',
         width: '100%',
+    },
+    runningPanel: {
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        ...{
+            ...shorthands.padding('24px'),
+        },
+        '&>*': {
+            maxWidth: '480px',
+        },
+        '&>ul': {
+            width: '100%',
+            display: 'flex',
+            flexDirection: 'column', 
+            alignItems: 'start',   
+        },
+        '&>.actions': {
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'end',
+        },
     }
 })
+function RunningProcessListItem({ e }: {
+    e: WorkspaceProcessState
+}) {
+    return (
+        <Button
+            appearance="transparent"
+            icon={e.state.icon}
+        >
+            {e.state.title}
+        </Button>
+    )
+}
 
-function ProssesRunningPanel() {
+function RunningProcessActions({ setIsNoRunning }: {
+    setIsNoRunning: (e: boolean) => void
+}) {
+    const dispatch = useSystemDispatch()
+    return (
+        <div className="actions">
+            <Button
+                appearance="primary"
+                onClick={() => {
+                    dispatch(updatePropertyForAllProcessState({
+                        properties: {
+                            requestClose: true
+                        }
+                    }))
+                    setIsNoRunning(true)
+                }}
+            >End All Processes</Button>
+        </div>
+    )
+}
+function RunningProcessList({ list }: {
+    list: WorkspaceProcessState[]
+}) {
+    return (
+        <ul>
+            {
+                list.map((e, i) => <RunningProcessListItem key={i} e={e}></RunningProcessListItem>)
+            }
+        </ul>
+    )
+}
+function ProssesRunningPanel({ setIsNoRunning }: {
+    setIsNoRunning: (e: boolean) => void
+}) {
+    const classes = useStyles()
+    const process = useSystemSelector(e => e.workspace.processStates)
+
+    useEffect(() => {
+        if(process.length === 0) {
+            setIsNoRunning(true)
+        }
+    }, [])
 
     return (
-        <></>
+        <div className={classes.runningPanel}>
+            <RunningProcessList list={process}></RunningProcessList>
+
+            <RunningProcessActions
+                setIsNoRunning={setIsNoRunning}
+            ></RunningProcessActions>
+        </div>
     )
 }
 
@@ -38,16 +127,15 @@ function SpinnerPanel() {
     const classes = useStyles()
 
     const dispatch = useDispatch()
-
     useEffect(() => {
-        const timer: NodeJS.Timeout | null = setTimeout(() => {
+        const timer = setTimeout(() => {
             dispatch(setReadyToShutdown(true))
-        }, 250)
+        }, 500)
 
         return () => {
             clearTimeout(timer)
         }
-    })
+    }, [])
 
     return (
         <div className={classes.spinnerPanel}>
@@ -64,15 +152,15 @@ function SpinnerPanel() {
 
 export function Shutdown() {
     const classes = useStyles()
-
-    const hasProssesRunning = false
+    const [isNoRunning, setIsNoRunning] = useState(false)
 
     return (
         <div className={classes.root}>
-
             {
-                hasProssesRunning ?
-                    <ProssesRunningPanel></ProssesRunningPanel> :
+                !isNoRunning ?
+                    <ProssesRunningPanel
+                        setIsNoRunning={setIsNoRunning}
+                    ></ProssesRunningPanel> :
                     <SpinnerPanel></SpinnerPanel>
             }
 
